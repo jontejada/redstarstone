93d73c356730a3b67cc5dce250613eece28e5b9a
jontejada <hi@jontejada.com>
2019-08-10 12:46:30 -0700

    Updates

---
+++ precache-manifest.c9cc5377763775faf687c7de244f5f9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b16575146ba45e8299891f99f46a2a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6c16ea9d0efd4764c6",
+    "url": "/static/css/2.d45e8b06.chunk.css"
+  },
+  {
+    "revision": "c7f4d40d23e137212e01",
+    "url": "/static/css/main.0f6b3dea.chunk.css"
+  },
+  {
+    "revision": "2e6c16ea9d0efd4764c6",
+    "url": "/static/js/2.6eda5268.chunk.js"
+  },
+  {
+    "revision": "c7f4d40d23e137212e01",
+    "url": "/static/js/main.5dbbb608.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  }
+]);
